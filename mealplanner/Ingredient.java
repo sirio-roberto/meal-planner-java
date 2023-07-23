5abc8e93b65896f807278c835b414ad6a3a2e7c8
Sirio <sirioroberto1@hotmail.com>
2023-07-23 18:29:17 -0300

    Created Meal and Ingredient entity classes and also simple menu to create a meal

---
+++ mealplanner/Ingredient.java
@@ -0,0 +1,14 @@
+package mealplanner;
+
+public class Ingredient {
+    private String name;
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ mealplanner/Meal.java
@@ -0,0 +1,41 @@
+package mealplanner;
+
+import java.util.List;
+
+public class Meal {
+    private String name;
+    private Category category;
+    private List<Ingredient> ingredients;
+
+    public Meal(String name, Category category, List<Ingredient> ingredients) {
+        this.name = name;
+        this.category = category;
+        this.ingredients = ingredients;
+    }
+
+    private String getIngredientsList() {
+        StringBuilder builder = new StringBuilder("Ingredients:");
+        ingredients.forEach(i -> builder.append("\n").append(i));
+        return builder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Category: %s
+                Name: %s
+                %s""",
+                category.toString(),
+                name,
+                getIngredientsList());
+    }
+
+    enum Category {
+        BREAKFAST, LUNCH, DINNER;
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+}
